f35a3ecb5bf4da946d914841b4a5018a95e60d6b
Ajit Jadhav <ajitjadhav701@gmail.com>
2023-08-09 15:14:53 +0530

    callback hell to bundling promises ,chaining

--- 3-Async-JavaScript/index.js
+++ 3-Async-JavaScript/index.js
@@ -1,15 +1,32 @@
 const fs = require('fs');
 const superagent = require('superagent');
 
-fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
-   console.log(`Breed: ${data}`);
-   superagent.get(`https://dog.ceo/api/breed/${data}/image/random`)
-      .end((err, res) => {
-         if (err) return console.log('error ::', err.message);
-
-            console.log(res?.body.message);
-            fs.writeFile(`imgaeDogs`, res.message,(err, res) => {
-               console.log('data saved in new file');
-            })
+const readFilePro = (file) => {
+   return new Promise((resolve, reject) => {
+      fs.readFile(file, (err, data) => {
+         if (err) reject('I could not find that file: ' + err.message);
+         resolve(data);
       })
+   })
+}
+
+const writeFilePro = (file, data) => {
+   return new Promise((resolve, reject) => {
+      fs.writeFile(file, (err, data) => {
+         if (err) reject('I could not find that file: ' + err.message);
+         resolve('success: ');
+      })
+   })
+}
+readFilePro(`${__dirname}/dog.txt`)
+   .then((data) => {
+   console.log(`Breed: ${data}`);   
+   return superagent.get(`https://dog.ceo/api/breed/${data}/image/random`)
+}).then(res => {
+   console.log(res?.body.message);
+   return fs.writeFile(`imgaeDogs.txt`, res.body.message)
+}).then(() => {
+   cconsole.log('image has been saved');
 })
+   .catch(err => console.log('error ::', err.message));
+ 
