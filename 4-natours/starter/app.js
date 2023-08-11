4f426fd54f89bf2057a057d9d843c11bc55fdb5b
Ajit Jadhav <ajitjadhav701@gmail.com>
2023-08-11 16:40:40 +0530

    handling post request and save data to file

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -2,6 +2,8 @@ const fs=require('fs');
 const express = require('express');
 const app = express();
 
+app.use(express.json());//middleware:between req and rex
+
 /*
 app.get('/',(req,res)=>{
     res.status(200).json({message:'hello from server side...!',status: 'OK'}); 
@@ -22,7 +24,24 @@ app.get('/api/v1/tours',(req,res)=>{
     });
 })
 
+
+app.post('/api/v1/tours',(req,res)=>{
+// console.log(req.body);
+    const newId=tours[tours.length-1].id+1;
+    const newTour=Object.assign({id:newId},req.body);
+    tours.push(newTour);
+    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours),err=>{
+        res.status(201).json({
+            status: 'success',
+            data:{
+                 tour:newTour
+            }
+        })
+    })
+})
 const port = 3000;
 app.listen(port, () => {
     console.log(`listening on port ${port}`);
 })
+
+
