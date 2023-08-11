8f1186030eb8dd6c1f6616dacda78b7005d95edb
Ajit Jadhav <ajitjadhav701@gmail.com>
2023-08-11 14:35:13 +0530

    starting my API -handling GET request

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -1,13 +1,25 @@
+const fs=require('fs');
 const express = require('express');
-
 const app = express();
 
+/*
 app.get('/',(req,res)=>{
     res.status(200).json({message:'hello from server side...!',status: 'OK'}); 
 });
-
 app.post('/',(req,res)=>{
     res.send('I am using post operation here..!')
+})
+*/
+
+const tours=JSON.parse( fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`))
+app.get('/api/v1/tours',(req,res)=>{
+    res.status(200).json({
+        status:'success',
+        results:tours.length,
+        data:{
+            tours,
+        }
+    });
 })
 
 const port = 3000;
